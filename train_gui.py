6faa41ea9d051dcf4e50b163bfa74b76c939e119
victorpere <paninaro095@gmail.com>
2026-09-08 15:00:27 -0400

    reverse fix; change speed to voltage

--- train_gui.py
+++ train_gui.py
@@ -12,13 +12,15 @@ class TrainController:
         self.root.title("N-Scale Train Controller")
         
         self.ser = None
-        self.direction = 1  # 1 for forward, -1 for backward
 
-        self.target_speed = tk.IntVar()  # selected target speed
-        self.target_speed.set(0)
+        self.direction = tk.IntVar()
+        self.direction.set(1)  # 1 for forward, -1 for backward
 
-        self.actual_speed = tk.IntVar()  # current speed from Arduino
-        self.actual_speed.set(0)
+        self.target_voltage = tk.IntVar()  # selected target voltage
+        self.target_voltage.set(0)
+
+        self.actual_voltage = tk.IntVar()  # current voltage from Arduino
+        self.actual_voltage.set(0)
 
         self.reading_serial = False  # Flag to control serial reading thread
         
@@ -39,40 +41,39 @@ class TrainController:
         self.status_label = ttk.Label(conn_frame, text="Disconnected", foreground="red")
         self.status_label.grid(row=0, column=3)       
 
+        # Dashboard frame
+        dash_font = tkFont.Font(family="Menlo", size=30)
+        dash_frame = ttk.LabelFrame(self.root, text="Voltage", padding = 10)
+        dash_frame.pack(fill="x", padx=10, pady=5)
+        ttk.Label(dash_frame, text="Target").grid(row=0, column=0)
+        ttk.Label(dash_frame, text="Actual").grid(row=0, column=1)
+        self.target_voltage_label = ttk.Label(dash_frame, textvariable=self.target_voltage, font=dash_font, width=3, justify=tk.RIGHT)
+        self.target_voltage_label.grid(row=1, column=0, padx=5)
+        self.actual_voltage_label = ttk.Label(dash_frame, textvariable=self.actual_voltage, font=dash_font, width=3, justify=tk.RIGHT)
+        self.actual_voltage_label.grid(row=1, column=1, padx=5)
         # Control frame
         control_frame = ttk.LabelFrame(self.root, text="Control", padding=10)
         control_frame.pack(fill="x", padx=10, pady=5)
         
         # Direction buttons
-        self.reverse_button = ttk.Button(control_frame, text="◀ REV", command=self.set_reverse, width=10)
-        self.reverse_button.grid(row=0, column=0, padx=5)
-        self.forward_button = ttk.Button(control_frame, text="FWD ▶", command=self.set_forward, width=10)
-        self.forward_button.grid(row=0, column=1, padx=5)
+        self.reverse_button = ttk.Button(control_frame, text="◀ REV", state="normal", command=self.set_reverse)
+        self.reverse_button.grid(row=0, column=0, padx=2)
+        self.forward_button = ttk.Button(control_frame, text="FWD ▶", state="disabled", command=self.set_forward)
+        self.forward_button.grid(row=0, column=1, padx=2)
 
-        # Preset speed buttons
-        self.stop_button = ttk.Button(control_frame, text="Stop", command=lambda: self.set_speed(0, self.stop_button))
+        # Preset voltage buttons
+        self.stop_button = ttk.Button(control_frame, text="STOP", command=lambda: self.set_voltage(0, self.stop_button))
         self.stop_button.grid(row=2, column=0, padx=2)
-        self.speed1_button = ttk.Button(control_frame, text="Station", command=lambda: self.set_speed(48, self.speed1_button))
-        self.speed1_button.grid(row=2, column=1, padx=2)
-        self.speed2_button = ttk.Button(control_frame, text="Slow", command=lambda: self.set_speed(70, self.speed2_button))
-        self.speed2_button.grid(row=2, column=2, padx=2)
-        self.speed3_button = ttk.Button(control_frame, text="1", command=lambda: self.set_speed(80, self.speed3_button))
-        self.speed3_button.grid(row=2, column=3, padx=2)
-        self.speed4_button = ttk.Button(control_frame, text="2", command=lambda: self.set_speed(96, self.speed4_button))
-        self.speed4_button.grid(row=2, column=4, padx=2)
-        self.speed5_button = ttk.Button(control_frame, text="3", command=lambda: self.set_speed(128, self.speed5_button))
-        self.speed5_button.grid(row=2, column=5, padx=2)
-
-        # # Dashboard frame
-        custom_font = tkFont.Font(family="Menlo", size=25)
-        dash_frame = ttk.LabelFrame(self.root, padding = 10)
-        dash_frame.pack(fill="x", padx=10, pady=5)
-        ttk.Label(dash_frame, text="Target speed").grid(row=0, column=0)
-        ttk.Label(dash_frame, text="Actual speed").grid(row=0, column=1)
-        self.target_speed_label = ttk.Label(dash_frame, textvariable=self.target_speed, background="#222", font=custom_font)
-        self.target_speed_label.grid(row=1, column=0, padx=5)
-        self.actual_speed_label = ttk.Label(dash_frame, textvariable=self.actual_speed, background="#222", font=custom_font)
-        self.actual_speed_label.grid(row=1, column=1, padx=5)
+        self.level1_button = ttk.Button(control_frame, text="IDLE", command=lambda: self.set_voltage(48, self.level1_button))
+        self.level1_button.grid(row=2, column=1, padx=2)
+        self.level2_button = ttk.Button(control_frame, text="1", command=lambda: self.set_voltage(70, self.level2_button))
+        self.level2_button.grid(row=2, column=2, padx=2)
+        self.level3_button = ttk.Button(control_frame, text="2", command=lambda: self.set_voltage(80, self.level3_button))
+        self.level3_button.grid(row=2, column=3, padx=2)
+        self.level4_button = ttk.Button(control_frame, text="3", command=lambda: self.set_voltage(96, self.level4_button))
+        self.level4_button.grid(row=2, column=4, padx=2)
+        self.level5_button = ttk.Button(control_frame, text="4", command=lambda: self.set_voltage(128, self.level5_button))
+        self.level5_button.grid(row=2, column=5, padx=2)
 
         # Message frame
         message_frame = ttk.LabelFrame(self.root, text="Messages", padding=10)
@@ -81,6 +82,7 @@ class TrainController:
         self.message_label.pack()
     
     def connect_serial(self):
+        self.set_message()
         try:
             port = self.port_var.get()
             self.ser = serial.Serial(port, 9600, timeout=1)
@@ -94,55 +96,56 @@ class TrainController:
             serial_thread.start()
         except Exception as e:
             self.connect_button.config(state="normal")
-            self.show_message("error", str(e))
+            self.set_message("error", str(e))
 
-    def set_speed(self, speed, button_pressed: Button):
-        if self.send_command(speed):
-            self.target_speed.set(speed)  # Store the speed           
+    def set_voltage(self, voltage, button_pressed: Button):
+        self.set_message()
+        directional_voltage = voltage * self.direction.get()
+        if self.send_command(directional_voltage):
+            self.target_voltage.set(voltage)  # Store the voltage           
             # Update button states
             self.reset_buttons()
             button_pressed.config(state="disabled")
     
-    def send_command(self, speed):
-        """Send speed command to Arduino."""
+    def send_command(self, voltage):
+        """Send voltage command to Arduino."""
         if not self.ser or not self.ser.is_open:
-            self.show_message("error", "Serial connection not established")
+            self.set_message("error", "Serial connection not established")
             self.connect_button.config(state="normal")
             self.status_label.config(text="Disconnected", foreground="red")
             self.reset_buttons()
             return False
         
-        pwm = speed
-        command = int(pwm * self.direction)  # Negative for reverse
+        command = int(voltage)  # Negative for reverse
         
         self.ser.write(f"{command}\n".encode())
         return True
     
     def set_forward(self):
-        if self.direction == 1:
-            self.show_message("info", "Already moving forward")
+        self.set_message()
+        if self.direction.get() == 1:
+            self.set_message("info", "Already moving forward")
             return  # Already moving forward
-        if self.target_speed != 0:
-            self.show_message("warning", "Can't change direction while moving")
+        if self.target_voltage.get() != 0 or self.actual_voltage.get() != 0:
+            self.set_message("warning", "Can't change direction while moving")
             return  # Can't change direction while moving
-        self.direction = 1
-        self.send_command(self.target_speed)  # Use stored speed
+        self.direction.set(1)
+        self.forward_button.config(state="disabled")
+        self.reverse_button.config(state="normal")
 
     def set_reverse(self):
-        if self.direction == -1:
-            self.show_message("info", "Already moving reverse")
+        self.set_message()
+        if self.direction.get() == -1:
+            self.set_message("info", "Already moving reverse")
             return  # Already moving reverse
-        if self.target_speed != 0:
-            self.show_message("warning", "Can't change direction while moving")
+        if self.target_voltage.get() != 0 or self.actual_voltage.get() != 0:
+            self.set_message("warning", "Can't change direction while moving")
             return  # Can't change direction while moving
-        self.direction = -1
-        self.send_command(self.target_speed)  # Use stored speed
+        self.direction.set(-1)
+        self.reverse_button.config(state="disabled")
+        self.forward_button.config(state="normal")
 
-    def stop(self):
-        self.target_speed.set(0)
-        self.send_command(0)
-
-    def show_message(self, level, message):
+    def set_message(self, level="", message=""):
         """Display a message in the message label with a given level (info, warning, error)."""
         if level == "info":
             self.message_label.config(text=message, foreground="blue")
@@ -154,7 +157,7 @@ class TrainController:
             self.message_label.config(text=message, foreground="black")
 
     def reset_buttons(self):
-        for btn in [self.stop_button, self.speed1_button, self.speed2_button, self.speed3_button, self.speed4_button, self.speed5_button]:
+        for btn in [self.stop_button, self.level1_button, self.level2_button, self.level3_button, self.level4_button, self.level5_button]:
             btn.config(state="normal")
 
     def read_serial(self):
@@ -164,10 +167,10 @@ class TrainController:
                 if self.ser.in_waiting > 0:
                     byte = self.ser.read(1)
                     if byte:
-                        self.actual_speed.set(byte[0])
+                        self.actual_voltage.set(byte[0])
                         # self.root.after(0, self.update_arduino_status)
             except Exception as e:
-                self.show_message("error", e)
+                self.set_message("error", e)
                 print(f"Serial read error: {e}")
             
             time.sleep(0.05)
